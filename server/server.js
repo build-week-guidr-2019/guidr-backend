1ebbe1274a7e09c780c6404a8d0b3ff42850f743
Ernesto Pena <ernestopenad@gmail.com>
2019-09-04 20:20:21 -0500

    comment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-
+//Server
 server.use(express.json());
 server.get('/' , (req , res) => {
     res.send('<h1>Back End Sprint Build- Guidr</h1>')
