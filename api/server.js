98f75bf5af82cd85e8106cf3def4990df185402d
arturolei <artie.lei@gmail.com>
2019-09-04 18:41:06 -0700

    Modified server.js

--- api/server.js
+++ api/server.js
@@ -9,3 +9,12 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+
+//added this to test that server was running.
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up' });
+  });
+
+
+module.exports = server;
